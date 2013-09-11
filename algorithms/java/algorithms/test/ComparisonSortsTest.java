582dd93211ca46d80861127aa7af998e74c0fe69
Кирилл Таран <kirill.t256@gmail.com>
2013-09-11 19:19:47 +0400

    MergeSort.

---
+++ algorithms/java/algorithms/test/ComparisonSortsTest.java
@@ -0,0 +1,36 @@
+package algorithms.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+import algorithms.MergeSort;
+import algorithms.ComparisonSortIface;
+
+public class ComparisonSortsTest {
+
+    private List<ComparisonSortIface<Integer>> getAllSorts() {
+        List<ComparisonSortIface<Integer>> sorts = new LinkedList<ComparisonSortIface<Integer>>();
+        sorts.add(new MergeSort());
+        return sorts;
+    }
+
+    @Test
+    public void test() {
+        for (ComparisonSortIface<Integer> sort : getAllSorts()) {
+            Integer[] array1 = {5,3,8,1,10,2,9,9,0};
+            Integer[] array2 = {10,9,8,1,2,3,7,6,5,4};
+            Integer[] array3 = {4};
+            Integer[] array4 = {};
+            sort.sort(array1);
+            sort.sort(array2);
+            sort.sort(array3);
+            sort.sort(array4);
+            assertEquals("[0, 1, 2, 3, 5, 8, 9, 9, 10]"   , Arrays.toString(array1));
+            assertEquals("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", Arrays.toString(array2));
+            assertEquals("[4]"                            , Arrays.toString(array3));
+            assertEquals("[]"                             , Arrays.toString(array4));
+        }
+    }
+}
