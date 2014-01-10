4a9da0697d9fa424d0bb18abacaa9c14a788d8b6
Kirill Taran <kirill.t256@gmail.com>
2014-01-10 19:55:09 +0400

    Generation of cycles.

--- multiagent/jade/AverageConsensus/run.sh
+++ multiagent/jade/AverageConsensus/run.sh
@@ -40,45 +40,88 @@ function tasks() {
         }
 
         function summary() {
-            cat .log | sed -n -e '/after/,$p' | sed s/.*\\r//g | grep -v "\=" > .temp
-            rm .log
+            error=`cat .log | grep "[eE]xception" | tr '\n' ' '`
+            if [ -z "$error" ]
+            then
+                cat .log | sed -n -e '/after/,$p' | sed s/.*\\r//g | grep -v "\=" > .temp
+                #rm .log
 
-            iterations=`cat .temp | head -1 | awk '{print $3}'`
-            proportion=`cat .temp | head -1 | awk '{print $7}' | sed s/\://g`
-            agents=`cat .temp | head -2 | tail -1 | awk '{print $3}'`
-            links=`cat .temp | head -3 | tail -1 | awk '{print $3}'`
-            messages=`cat .temp | head -4 | tail -1 | awk '{print $3}'`
-            rm .temp
+                iterations=`cat .temp | head -1 | awk '{print $3}'`
+                proportion=`cat .temp | head -1 | awk '{print $7}' | sed s/\://g`
+                agents=`cat .temp | head -2 | tail -1 | awk '{print $3}'`
+                links=`cat .temp | head -3 | tail -1 | awk '{print $3}'`
+                messages=`cat .temp | head -4 | tail -1 | awk '{print $3}'`
+                rm .temp
 
-            cost=`echo "$link_cost * $links + $message_cost * $messages" | bc`
-            echo -n "Input: $iterations iterations; proportion $proportion; "
-            echo -n "$agents agents; $links links."
-            echo    "Sended $messages messages. Cost of topology: $cost"
+                cost=`echo "$link_cost * $links + $message_cost * $messages" | bc`
+                echo -n "Input: $iterations iterations; proportion $proportion; "
+                echo -n "$agents agents; $links links. "
+                echo    "Sended $messages messages. Cost of topology: $cost"
+            else
+                echo "Failed."
+                cat  .log  >> .failed
+                echo "***" >> .failed
+            fi
         }
         
         launch $(args $@) &> .log
         summary
     }
 
-    limit=10
+    limit=1000
     proportion=0.5
+
+    function manager() {
+        echo -n "manager \"manager\" $limit $proportion"
+        for i in `seq 1 $1`
+        do
+            echo -n " agent$i"
+        done
+        echo -n " !"
+    }
+
+    function cycle()  {
+        k=$1 # max number
+        p=$k # previous number
+        for i in $(seq 1 $(expr $k - 1))
+        do
+            n=$(expr $i + 1) # next number
+            echo -n "agent$i $RANDOM \"<-agent$p\" \"->agent$n\" ! "
+            p=$i
+        done
+        echo -n "agent$k $RANDOM \"<-agent$p\" \"->agent1\" ! "
+        manager $n
+    }
+
+    function full() {
+        n=$1
+    }
+
+    rm .failed &> /dev/null
 
     echo "Cost of link: $link_cost; cost of message: $message_cost."
 
-    average \
-        x 5.123 "<-z" "->y" ! \
-        y 6.96  "<-x" "->z" ! \
-        z 8.2   "<-y" "->x" ! \
-        manager "manager" $limit $proportion "x" "y" "z" !
+    for i in `seq 1 100`
+    do
+        echo -n "Topology: cycle of $i vertices. "
+        average `cycle $i`
+    done
 
-    average \
-        q 5.123 "<-z"       "->w"       ! \
-        w 9.96  "<-q" "<-x" "->y" "->z" ! \
-        x 6.78  "<-y" "<-z" "->w"       ! \
-        y 8.2   "<-w"       "->x"       ! \
-        z 0.5   "<-w"       "->q" "->x" ! \
-        manager "manager" $limit $proportion "q" "w" "x" "y" "z" !
+    #average \
+        #x 5.123 "<-z" "->y" ! \
+        #y 6.96  "<-x" "->z" ! \
+        #z 8.2   "<-y" "->x" ! \
+        #manager "manager" $limit $proportion "x" "y" "z" !
+
+    #average \
+        #q 5.123 "<-z"       "->w"       ! \
+        #w 9.96  "<-q" "<-x" "->y" "->z" ! \
+        #x 6.78  "<-y" "<-z" "->w"       ! \
+        #y 8.2   "<-w"       "->x"       ! \
+        #z 0.5   "<-w"       "->q" "->x" ! \
+        #manager "manager" $limit $proportion "q" "w" "x" "y" "z" !
     echo "Done."
 }
 
+> tasks.log
 tasks &> tasks.log & tail -f tasks.log
