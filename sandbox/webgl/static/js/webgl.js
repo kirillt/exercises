34c0d5c071036d699cfa3a8e9f9e719c5df36796
Kirill Taran <kirill.t256@gmail.com>
2018-07-23 16:00:51 +0300

    Little JS refactoring

--- sandbox/webgl/static/js/webgl.js
+++ sandbox/webgl/static/js/webgl.js
@@ -1,9 +1,9 @@
-var canvas = document.getElementById('frame');
-var gl = canvas.getContext('webgl');
-var program;
+const canvas = document.getElementById('frame');
+const gl = canvas.getContext('webgl');
+let program;
 
 const InitWebGL = function() {
-    var VSText, FSText;
+    let VSText, FSText;
     loadTextResource('shaders/vertexShader.glsl')
         .then(function(result) {
             VSText = result;
@@ -17,7 +17,7 @@ const InitWebGL = function() {
             alert('Error while loading resources');
             console.error(error);
         });
-}
+};
 
 const StartWebGL = function(vertexShaderText, fragmentShaderText) {
     if (!gl) {
@@ -31,7 +31,7 @@ const StartWebGL = function(vertexShaderText, fragmentShaderText) {
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
     function compile(typ, source) {
-        var shader = gl.createShader(typ);
+        let shader = gl.createShader(typ);
 
         gl.shaderSource(shader, source);
         gl.compileShader(shader);
@@ -43,8 +43,8 @@ const StartWebGL = function(vertexShaderText, fragmentShaderText) {
         return shader;
     }
 
-    var vertexShader = compile(gl.VERTEX_SHADER, vertexShaderText);
-    var fragmentShader = compile(gl.FRAGMENT_SHADER, fragmentShaderText);
+    let vertexShader = compile(gl.VERTEX_SHADER, vertexShaderText);
+    let fragmentShader = compile(gl.FRAGMENT_SHADER, fragmentShaderText);
 
     program = gl.createProgram();
     gl.attachShader(program, vertexShader);
@@ -74,7 +74,7 @@ const flatten = function(arr, result = []) {
 };
 
 const move = function(points, vector) {
-    var result = new Array();
+    let result = [];
     for (i = 0; i < points.length; i++) {
     result.push({
             x: points[i].x + vector.x,
@@ -82,10 +82,10 @@ const move = function(points, vector) {
         });
     }
     return result;
-}
+};
 
 const stretch = function(points, q) {
-    var result = new Array();
+    let result = [];
     for (i = 0; i < points.length; i++) {
         result.push({
             x: points[i].x * q,
@@ -93,16 +93,16 @@ const stretch = function(points, q) {
         });
     }
     return result;	
-}
+};
 
 const from_angles = function(segments) {
-    var x = 0.0;
-    var y = 0.0;
-    var a = 0.0;
+    let x = 0.0;
+    let y = 0.0;
+    let a = 0.0;
 
-    var result = new Array();
-    for (i = 0; i < segments.length; i++) {
-        var r = segments[i].length;
+    let result = [];
+    for (let i = 0; i < segments.length; i++) {
+        let r = segments[i].length;
 
         a = a + segments[i].angle;
         x = x + r * Math.cos(a);
@@ -111,21 +111,21 @@ const from_angles = function(segments) {
         result.push({x: x, y: y});
     }
     return result;
-}
+};
 
 const polygon = function(n) {
-    var radius = 1 / (2 * Math.sin(Math.PI / n));
-    var figure = new Array({angle: 0, length: radius});
+    let radius = 1 / (2 * Math.sin(Math.PI / n));
+    let figure = new Array({angle: 0, length: radius});
 
-    var angle = 2 * Math.PI / n;
-    for (i = 0; i < n; i++) {
+    let angle = 2 * Math.PI / n;
+    for (let i = 0; i < n; i++) {
         figure.push({angle: angle, length: 1});
     }
     return from_angles(figure);
-}
+};
 
 const triangulate_2d = function(contour) {
-    var n = contour.length;
+    let n = contour.length;
     if (n < 3) {
         alert("Can't triangulate " + n + " vertices");
         return undefined;
@@ -134,8 +134,8 @@ const triangulate_2d = function(contour) {
     	// return countour;
     // }
 
-    var center = {x: 0, y: 0};
-    for (i = 0; i < n; i++) {
+    let center = {x: 0, y: 0};
+    for (let i = 0; i < n; i++) {
         center = {
             x: center.x + contour[i].x,
             y: center.y + contour[i].y
@@ -146,34 +146,34 @@ const triangulate_2d = function(contour) {
         y: center.y / n
     };
 
-    var result = new Array();
-    for (i = 0; i < n - 1; i++) {
+    let result = [];
+    for (let i = 0; i < n - 1; i++) {
         result = result.concat([contour[i], contour[i+1], center]);
     }
     result = result.concat([contour[n-1], contour[0], center]);
 
     return result;
-}
+};
 
 const random_color = function() {
     return [Math.random(), Math.random(), Math.random()];
-}
+};
 
-var n = 0;
-var init = [];
+let n = 0;
+let init = [];
 {
-    var figures = [
+    const figures = [
         move(stretch(polygon(9), 0.4), {x: -0.4, y: -0.4}),
         move(stretch(polygon(7), 0.4), {x: -0.3, y: -0.3}),
         move(stretch(polygon(5), 0.3), {x: -0.2, y: -0.2}),
         move(stretch(polygon(3), 0.2), {x: -0.1, y: -0.1}),
     ];
 
-    var z = 0.0;
-    var points = flatten(figures.map(f => {
+    let z = 0.0;
+    const points = flatten(figures.map(f => {
         z = z - 0.05;
-        var color = random_color();
-        var triangulation = triangulate_2d(f);
+        const color = random_color();
+        let triangulation = triangulate_2d(f);
         triangulation.forEach(p => {
             p.z = z;
             p.rgb = color;
@@ -186,29 +186,29 @@ var init = [];
     init = flatten(points.map(p => [p.x, p.y, p.z].concat(p.rgb)));
 }
 
-var extra = [];
+let extra = [];
 
 const draw = function() {
-    var vertexBuffer = gl.createBuffer();
+    const vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 
-    var points = init.concat(extra);
-    var k = extra.length / 6;
+    const points = init.concat(extra);
+    let k = extra.length / 6;
 
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(points), gl.STATIC_DRAW);
 
-    var positionAttribLocation = gl.getAttribLocation(program, 'vertexPosition');
+    let positionAttribLocation = gl.getAttribLocation(program, 'vertexPosition');
     gl.vertexAttribPointer(
         positionAttribLocation,
         3,
         gl.FLOAT,
         gl.FALSE,
         6 * Float32Array.BYTES_PER_ELEMENT,
-        0 * Float32Array.BYTES_PER_ELEMENT
+        0
     );
     gl.enableVertexAttribArray(positionAttribLocation);
 
-    var colorAttribLocation = gl.getAttribLocation(program, 'vertexColor');
+    let colorAttribLocation = gl.getAttribLocation(program, 'vertexColor');
     gl.vertexAttribPointer(
         colorAttribLocation,
         3,
@@ -235,16 +235,16 @@ const draw = function() {
     gl.drawArrays(gl.TRIANGLES, 0, n + k);
     gl.drawArrays(gl.POINTS, 0, n + k);
     gl.drawArrays(gl.LINE_STRIP, 0, n + k);
-}
+};
 
 canvas.onmousedown = function onmousedown(event) {
-    var middle_X = gl.canvas.width / 2;
-    var middle_Y = gl.canvas.height / 2;
+    const middle_X = gl.canvas.width / 2;
+    const middle_Y = gl.canvas.height / 2;
 
-    var rect = canvas.getBoundingClientRect();
+    const rect = canvas.getBoundingClientRect();
 
-    var x = (event.clientX - rect.left - middle_X) / middle_X;
-    var y = (middle_Y - event.clientY + rect.top) / middle_Y;
+    const x = (event.clientX - rect.left - middle_X) / middle_X;
+    const y = (middle_Y - event.clientY + rect.top) / middle_Y;
     console.log("new point at (" + x + ", " + y + ")");
 
     extra = extra.concat([x, y, -0.1]);
